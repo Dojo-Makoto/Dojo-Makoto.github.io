71fe24a2bd5ed37634b460d382f68acc5cd67cd0
Nicolas BONNET <nicolas.bonnet07@gmail.com>
2021-02-19 20:35:39 +0100

    First commit

---
+++ index_en.php
@@ -0,0 +1,267 @@
+<!DOCTYPE HTML>
+<!--
+	Ex Machina by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Aïkido Dojo Makoto Marseille | Homepage | English</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="Web site of Aikido Dojo Makoto, Dojo of André Palmeri (6th dan - student of Tamura Senseï) located in Marseille 9th arrondissement - Homepage" />
+		<meta name="keywords" content="Dojo
+Aikido
+Marseille
+Club
+Andre Palmeri
+6eme dan
+adultes
+initiation
+FFAB
+Mazargues
+13009
+Luminy
+Arts
+Arts martiaux
+Budo
+Tatami
+Bokken
+Jo
+Armes
+Kamiza
+Kimono
+Keiko gi
+Zori
+Développement personnel
+Hakama
+Tamura
+Tamura Sensei
+Maître Tamura
+Teens
+Teenagers
+Children
+Kids" />
+		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:700italic,400,300,700' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<style type="text/css">
+<!--
+.Style1 {font-weight: bold}
+.Style2 {font-weight: bold}
+-->
+        </style>
+<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-desktop.css" />
+			<link rel="stylesheet" href="css/makoto.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+        <link rel="stylesheet" href="../libs/flexslider/flexslider.css" type="text/css" />
+		<style type="text/css">
+        .flexslider .slides img{
+            width: auto; /* undo width: 100%  in flexslider.css*/
+            max-width:100%;  /* Make sure the image stays inside its surrounding container. */
+            height: auto; /* keep the aspect ratio */
+            margin-left:auto;
+            margin-right:auto;
+            margin-top:0px;
+            margin-bottom:0px;
+			max-height:400px;
+        }
+</style>
+	</head>
+	<body class="homepage">
+    
+    <!-- jQuery -->
+  	<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\x3C/script>')</script>-->
+
+  	<!-- FlexSlider config -->
+  	<script defer="defer" src="../libs/flexslider/jquery.flexslider.js"></script>
+
+  	<script charset="utf-8" type="text/javascript">
+   	$(window).load(function() {
+      	$('.flexslider').flexslider({
+        	animation: "fade",
+			slideshowSpeed: 4000,
+        animationSpeed: 1000
+      	});
+   	});
+	</script>
+
+	<!-- Header -->
+		<div id="header">
+			<div class="container">
+					
+				<!-- Logo -->
+					<div id="logo">
+						<!-- <h1><a href="#"><img src="images/logo_makoto.png" alt="Dojo d'Aïkido Makoto"></a></h1> -->
+			  		</div>
+				
+		<!-- Nav -->
+					
+                    
+                    <nav id="nav">
+						<ul>
+							<li class="active"><a href="index_en.php">Homepage</a></li>
+                            <li><a href="discipline_en.html">The discipline</a></li>
+							<li><a href="#">The teaching</a>
+                            	<ul class="hidden">
+                                	<li><a href="professeur_en.html">The teacher</a></li>
+                                    <li><a href="uchi_en.html">The uchi deshi</a></li>
+                                </ul>
+                            </li>
+							<li><a href="#">The Dojo &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+                            	<ul class="hidden">
+                                	<li><a href="fonctionnement_en.html">Functioning</a></li>
+                                    <li><a href="galerie_en.html">Gallery</a></li>
+                                </ul>
+                            </li>
+							<li><a href="#">Join us</a>
+                            	<ul class="hidden">
+                                	<li><a href="pratique_en.html#horaires">Classes</a></li>
+                                      <li><a href="pratique_en.html#acces">Access</a></li>
+                                      <li><a href="pratique_en.html#inscriptions">Registration</a></li>
+                                      <li><a href="pratique_en.html#contributions">Contributions</a></li>
+                                      <li><a href="pratique_en.html#horaires">Youths</a></li>
+                                      <li><a href="pratique_en.html#visitors">Visitors</a></li>
+                                      <li><a href="pratique_en.html#contact">Contact</a></li>
+                                      <li><a href="actualite_en.html">News</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="index.php"><img src="images/frenchflag.png" alt="EN"></a><a href="https://www.facebook.com/pages/Dojo-dAikido-Makoto/249274268454884"><img src="images/facebook-logo.jpg" alt="Makoto Facebook"></a></li>
+						</ul>
+
+					</nav>
+			</div>
+		</div>
+	<!-- Header -->
+		
+	<!-- Banner -->
+		<div id="banner">
+        	<!-- flexslider -->
+			
+            <!-- <div class="flexslider" id="slideshow" style="width:900px;height:427px;margin:auto">-->
+            <!-- <div class="flexslider" id="slideshow" style="width:100%;height:auto;margin:auto">-->
+            <div class="flexslider" id="slideshow" style="width:100%;height:auto;margin:auto">
+                <ul class="slides">
+                    <li>
+                        <img src="images/slider/zafu.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/groupe_seiza.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/fab.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/andre2.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/bokken.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/groupe_armes1.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/andre4.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/andre1.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/seiza_hakama.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/seiza_laurent2.jpg" />
+                    </li>
+                    <li>
+                      <img src="images/slider/arbre.jpg" />
+                    </li>
+                </ul>
+	</div><!-- flexslider -->
+			
+		</div>
+	<!-- /Banner -->
+
+	<!-- Main -->
+		<div id="page">
+
+			<!-- Extra -->
+			<div id="marketing" class="container">
+				<div class="row">
+                	<div class="2u">
+                    <!-- <div id="aikido_kanji"> -->
+                        <img src="images/aikido-kanji.gif" alt="Aikido">
+                    <!-- </div> -->
+					</div>
+					<div class="3u" >
+						<section>
+							<header>
+								<h2>Aikido</h2>
+							</header>
+
+							<p class="subtitle" style="height:10em;">We are members of the FFAB (French Aikido and Budo Federation) and of the current Aikikai, founded and developed in France by <strong>Tamura Sensei</strong>. 
+                           <br>
+                           <a href="https://youtu.be/nG7Tno7yv9U" target="_blank">Few images...</a> </p>
+						  <p><a href="discipline_en.html"><img src="images/photo_sensei_small.jpg" alt=""></a></p>
+						  <a href="discipline_en.html" class="button" >More</a>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<header>
+								<h2>News</h2>
+							</header>
+                            
+                            <p class="subtitle" style="height:10em;">Latest news of the Dojo, agenda, events&nbsp;...
+                            </p>
+							<p><a href="actualite_en.html"><img src="images/photo_actu_small.jpg" alt=""></a></p>
+						  <a href="actualite_en.html" class="button">More</a>
+						</section>
+					</div>
+					<div class="3u">
+						<section>
+							<header>
+								<h2>The teacher</h2>
+							</header>
+							<p class="subtitle" style="height:10em;"><strong>André Palmeri</strong> - 6th dan, National Coaching Qualification.
+                            
+                            André Palmeri began Aïkido in the 70's. Student of Claude Pellerin, he followed the teaching of Tamura Senseï and Chiba Senseï.</p>
+							<p><a href="professeur_en.html"><img src="images/photo_andre_small.jpg" alt=""></a></p>
+						  <a href="professeur_en.html" class="button">More</a> 
+						</section>
+					</div>
+					
+				</div>
+			</div>
+			<!-- /Extra -->
+				
+			<!-- Main -->
+			
+			<!-- Main -->
+
+		</div>
+	<!-- /Main -->
+
+	<!-- Featured -->
+
+	<!-- /Featured -->
+
+	<!-- Footer -->
+
+	<!-- /Footer -->
+
+	<!-- Copyright -->
+		<div id="copyright" class="container">
+			Dojo d'Aïkido Makoto - 47, av. de Lattre de Tassigny, 13009 Marseille - <a href="mailto:makoto.dojo@free.fr">makoto.dojo@free.fr</a>
+		</div>
+
+
+	</body>
+</html>
