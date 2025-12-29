96a64c3e5bf17c8be9a858a42dbb4f15492aaa44
Nicolas BONNET <nicolas.bonnet@univ-amu.fr>
2025-12-29 22:58:23 +0100

    add popup in index.html to display discover week in 2026

---
+++ js/discover-week-2026.js
@@ -0,0 +1,33 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const popup = document.getElementById('semaineDecouvertePopup');
+    const popupContent = document.querySelector('.popup-content');
+    const closeBtn = document.querySelector('.close-btn');
+    const ctaBtn = document.querySelector('.cta-btn');
+    
+    setTimeout(() => {
+        popup.style.display = 'flex';
+    }, 2000);
+    
+    closeBtn.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+    
+    popup.addEventListener('click', function(event) {
+        if (event.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+    
+    popupContent.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
+    
+    ctaBtn.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
+
+    setTimeout(() => {
+        popup.style.display = 'none';
+    }, 12000);
+});
